24a5cf1267e43e22bbcb359dfdf4790d054b24de
xuan <xuanqinjian@gmail.com>
2021-01-25 16:19:59 +0800

    优化插入排序

--- src/com/xuan/algorithm/algorithm_java/sort/InsertionSort.java
+++ src/com/xuan/algorithm/algorithm_java/sort/InsertionSort.java
@@ -37,6 +37,17 @@ public class InsertionSort {
             arr[j] = t;
         }
     }
+    //反向排序
+    public static <E extends Comparable<E>> void sort2(E[] arr) {
+        for (int i = arr.length - 1; i >= 0; i--) {
+            E t = arr[i];
+            int j;
+            for (j = i; j + 1 < arr.length && t.compareTo(arr[j + 1]) > 0; j++) {
+                arr[j] = arr[j + 1];
+            }
+            arr[j] = t;
+        }
+    }
 
     public static <E extends Comparable<E>> void swap(E[] arr, int i, int j) {
         E t = arr[i];
@@ -49,7 +60,7 @@ public class InsertionSort {
         int[] dataSize = {10000, 100000};
         for (int n : dataSize) {
             Integer[] arr = ArrayGenerator.generateRandomArray(n, n);
-            Integer[] arr2 = Arrays.copyOf(arr,arr.length);
+            Integer[] arr2 = Arrays.copyOf(arr, arr.length);
             SortingHelper.sortTest("SelectionSort", arr);
             SortingHelper.sortTest("InsertionSort", arr2);
 
