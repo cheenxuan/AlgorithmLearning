baf290c812f2b20f158e9b47ebedbafc8afe4796
xuan <xuanqinjian@gmail.com>
2021-01-25 15:38:11 +0800

    优化插入排序

--- src/com/xuan/algorithm/algorithm_java/SortingHelper.java
+++ src/com/xuan/algorithm/algorithm_java/SortingHelper.java
@@ -29,6 +29,8 @@ public class SortingHelper {
             SelectionSort.sort(arr);
         else if(sortName.equals("InsertionSort"))
             InsertionSort.sort(arr);
+        else if(sortName.equals("InsertionSort2"))
+            InsertionSort.sort2(arr);
         long endTime = System.nanoTime();
 
         double time = (endTime - startTime) / 1000000000.0;
--- src/com/xuan/algorithm/algorithm_java/sort/InsertionSort.java
+++ src/com/xuan/algorithm/algorithm_java/sort/InsertionSort.java
@@ -3,6 +3,8 @@ package com.xuan.algorithm.algorithm_java.sort;
 import com.xuan.algorithm.algorithm_java.ArrayGenerator;
 import com.xuan.algorithm.algorithm_java.SortingHelper;
 
+import java.util.Arrays;
+
 public class InsertionSort {
 
     public InsertionSort() {
@@ -17,6 +19,22 @@ public class InsertionSort {
                     swap(arr, j, j - 1);
                 else break;
             }
+
+            for (int j = i; j - 1 >= 0 && arr[j].compareTo(arr[j - 1]) < 0; j--)
+                swap(arr, j, j - 1);
+        }
+    }
+
+    public static <E extends Comparable<E>> void sort2(E[] arr){
+
+        for (int i = 0; i < arr.length; i++) {
+            //将arr[i]插入到合适的位置
+            E t = arr[i];
+            int j;
+            for (j = i; j -1 >= 0 && t.compareTo(arr[j - 1]) < 0; j--) {
+                arr[j] = arr[j -1];
+            }
+            arr[j] = t;
         }
     }
 
@@ -31,8 +49,9 @@ public class InsertionSort {
         int[] dataSize = {10000, 100000};
         for (int n : dataSize) {
             Integer[] arr = ArrayGenerator.generateRandomArray(n, n);
-            SortingHelper.sortTest("InsertionSort",arr);
+            Integer[] arr2 = Arrays.copyOf(arr,arr.length);
+            SortingHelper.sortTest("InsertionSort", arr);
+            SortingHelper.sortTest("InsertionSort2", arr2);
         }
-
     }
 }
