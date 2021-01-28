2d57632c9f6b8ba17c09f360412704f92181a3a3
xuan <xuanqinjian@gmail.com>
2021-01-28 17:46:50 +0800

    添加数据结构-循环队列

--- src/com/xuan/algorithm/Main.java
+++ src/com/xuan/algorithm/Main.java
@@ -1,18 +1,38 @@
 package com.xuan.algorithm;
 
-import com.xuan.algorithm.datastructure.ArrayStack;
+import com.xuan.algorithm.datastructure.ArrayQueue;
+import com.xuan.algorithm.datastructure.LoopQueue;
+import com.xuan.algorithm.datastructure.Queue;
+
+import java.util.Random;
 
 public class Main {
 
+    private static double testQueue(Queue<Integer> q,int opCount){
+        long startTime = System.nanoTime();
+
+        Random random = new Random();
+        for (int i = 0; i < opCount; i++) {
+            q.enqueue(random.nextInt(Integer.MAX_VALUE));
+        }
+        for (int i = 0; i < opCount; i++) {
+            q.dequeue();
+        }
+
+        long endTime = System.nanoTime();
+        return (endTime - startTime) / 1000000000.0;
+    }
+
     public static void main(String[] args) {
 
-        ArrayStack<Integer> stack = new ArrayStack<>();
-        for (int i = 0; i < 5; i++) {
-            stack.push(i);
-        }
-        System.out.println(stack);
+        int opCount = 100000;
+        ArrayQueue<Integer> arrayQueue = new ArrayQueue<>();
+        double time1 = testQueue(arrayQueue,opCount);
+        System.out.println("ArrayQueue, time: " +time1+" s");
 
-        stack.pop();
-        System.out.println(stack);
+        LoopQueue<Integer> loopQueue = new LoopQueue<>();
+        double time2 = testQueue(loopQueue,opCount);
+        System.out.println("LoopQueue, time: " + time2 + " s");
+
     }
 }
